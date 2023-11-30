4d4cbda0d66f1b0d48a4ebf76a4a516f310c80ec
Jaime <87080231+jaimeVillagran@users.noreply.github.com>
2023-11-30 20:47:50 -0300

    Refactor code to add ThemeContext and implement dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import { Routes, Route } from 'react-router-dom'
-import Header from './atoms/Header/Header'
-import Songs from './molecules/Songs/Songs'
-import Lyrics from './molecules/Lyrics/Lyrics'
-import NotFound from './molecules/NotFound/NotFound'
-import './assets/css/styles.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './contexts/ThemeContext/ThemeContext';
+import Header from './atoms/Header/Header';
+import Songs from './molecules/Songs/Songs';
+import Lyrics from './molecules/Lyrics/Lyrics';
+import NotFound from './molecules/NotFound/NotFound';
+import './assets/css/styles.css';
 
-const App = () => (
-  <>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Songs />} />
-      <Route path="/lyrics/track/:commontrack_id" element={<Lyrics />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </>
-)
+const App = () => {
+	return (
+		<ThemeProvider>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Songs />} />
+				<Route path="/lyrics/track/:commontrack_id" element={<Lyrics />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</ThemeProvider>
+	);
+};
 
-export default App
+export default App;
